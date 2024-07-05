15c34f971b28a6f57049e7f4b26764087dc1513b
sevepeoplleh <sevepeoplleh@gmail.com>
2024-07-04 21:24:23 -0400

    First HW is done.

--- src/main/java/org/example/e1/E1BooleanPrinter.java
+++ src/main/java/org/example/e1/E1BooleanPrinter.java
@@ -2,6 +2,11 @@ package org.example.e1;
 
 public class E1BooleanPrinter {
     public static void main(String[] args) {
+        boolean isTrue = true;
+        boolean isFalse = false;
+
+        System.out.println(isTrue);
+        System.out.println(isFalse);
 
 
 
--- src/main/java/org/example/e4/E4CreatingVariablesAndPrinting.java
+++ src/main/java/org/example/e4/E4CreatingVariablesAndPrinting.java
@@ -2,6 +2,14 @@ package org.example.e4;
 
 public class E4CreatingVariablesAndPrinting {
     public static void main(String[] args) {
+        String char1 = "*";
+        String char2 = "#";
+
+        System.out.println(char1);
+        System.out.println(char1+char1);
+        System.out.println(char1+char1+char1);
+        System.out.println(char2+char2);
+        System.out.println(char2);
 
     }
 }
